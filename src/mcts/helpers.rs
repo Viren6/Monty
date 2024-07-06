0ec911e756c292d6d14d0d410e0bb4a1573c583d
Viren6 <94880762+Viren6@users.noreply.github.com>
2024-07-06 01:34:02 +0100

    Take 4 fast
    bench 2304194

--- src/mcts/helpers.rs
+++ src/mcts/helpers.rs
@@ -18,8 +18,8 @@ impl SearchHelpers {
 
         // scale CPUCT with variance of Q
         if parent.visits() > 1 {
-            let frac = pow(parent.var(), 0.52) / params.cpuct_var_scale();
-            cpuct *= 1.0 + params.cpuct_var_weight() * (frac - 1.0);
+            let frac = parent.var().sqrt() / params.cpuct_var_scale();
+            cpuct *= pow(1.0 + params.cpuct_var_weight() * (frac - 1.0), 0.95);
         }
 
         cpuct
