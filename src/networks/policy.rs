deca5861d701668d1ffe0f908fc0741b7ff7fc13
Viren6 <94880762+Viren6@users.noreply.github.com>
2024-11-10 16:59:31 +0000

    Policy pairwise
    bench 1741694

--- src/networks/policy.rs
+++ src/networks/policy.rs
@@ -10,13 +10,13 @@ use super::{
 
 // DO NOT MOVE
 #[allow(non_upper_case_globals)]
-pub const PolicyFileDefaultName: &str = "nn-5e45d73042ed.network";
+pub const PolicyFileDefaultName: &str = "nn-e04df0b6b979.network";
 
 const QA: i16 = 256;
 const QB: i16 = 512;
 const FACTOR: i16 = 32;
 
-pub const L1: usize = 4096;
+pub const L1: usize = 6144;
 
 #[repr(C)]
 #[derive(Clone, Copy)]
