d001023eace372c17d8a7f754403d1ac88b04bf2
Viren6 <94880762+Viren6@users.noreply.github.com>
2024-11-12 16:20:35 +0000

    Policygen11
    bench 2357713

--- src/networks/policy.rs
+++ src/networks/policy.rs
@@ -10,7 +10,7 @@ use super::{
 
 // DO NOT MOVE
 #[allow(non_upper_case_globals)]
-pub const PolicyFileDefaultName: &str = "nn-a27f352d4a03.network";
+pub const PolicyFileDefaultName: &str = "nn-c1d16399b5ae.network";
 
 const QA: i16 = 256;
 const QB: i16 = 512;
