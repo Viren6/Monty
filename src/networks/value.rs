9baadc32388aeb4c61cbb34ef5433403db5293ab
Viren6 <94880762+Viren6@users.noreply.github.com>
2024-12-16 00:25:16 +0000

    New net
    bench 1698990

--- src/networks/value.rs
+++ src/networks/value.rs
@@ -8,7 +8,7 @@ use super::{
 
 // DO NOT MOVE
 #[allow(non_upper_case_globals)]
-pub const ValueFileDefaultName: &str = "nn-68b9a835a698.network";
+pub const ValueFileDefaultName: &str = "nn-23f62a5c6cdd.network";
 
 const QA: i16 = 512;
 const QB: i16 = 1024;
