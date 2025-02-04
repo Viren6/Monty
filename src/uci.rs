036836a6f554fd02aa723f9550d016a44cefcb40
Viren6 <94880762+Viren6@users.noreply.github.com>
2025-02-04 09:35:20 +0000

    Update uci.rs

--- src/uci.rs
+++ src/uci.rs
@@ -84,8 +84,7 @@ pub fn run(policy: &PolicyNetwork, value: &ValueNetwork) {
             "perft" => run_perft(&commands, &pos),
             "quit" => std::process::exit(0),
             "eval" => {
-                println!("cp: {}", pos.get_value(value, &params));
-                println!("wdl: {:.2}%", 100.0 * pos.get_value_wdl(value, &params));
+                println!("{}", pos.get_value(value, &params));
                 io::stdout().flush().expect("Failed to flush stdout");
             }
             "policy" => {
