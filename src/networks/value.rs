64b065132eb22d385a89e959abd175e311545c99
Viren6 <94880762+Viren6@users.noreply.github.com>
2025-08-28 22:53:41 +0100

    hm

--- src/networks/value.rs
+++ src/networks/value.rs
@@ -8,7 +8,7 @@ use super::{
 
 // DO NOT MOVE
 #[allow(non_upper_case_globals, dead_code)]
-pub const ValueFileDefaultName: &str = "sb800-fixed-threats.network";
+pub const ValueFileDefaultName: &str = "sb1000-fixed-threats-no-piece-if-threats.network";
 #[allow(non_upper_case_globals, dead_code)]
 pub const CompressedValueName: &str = "nn-f004da0ebf25.network";
 #[allow(non_upper_case_globals, dead_code)]
