3281be1b920de6d40f072b829faf63195d07dcd4
Viren6 <94880762+Viren6@users.noreply.github.com>
2025-09-02 12:57:52 +0100

    Take 7
    bench 1332295

--- src/mcts/iteration.rs
+++ src/mcts/iteration.rs
@@ -126,6 +126,7 @@ fn pick_action(searcher: &Searcher, ptr: NodePtr, node: &Node) -> usize {
         acc += searcher.tree[actions_ptr + k].policy();
         k += 1;
     }
+    k += 4;
     let mut thresh = 1;
     while node.visits() >= thresh && k < node.num_actions() {
         k += 1;
